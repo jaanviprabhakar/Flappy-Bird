6c2ea51349f014d89cd82f0a0be9cfac156ae4a3
Jaanvi Prabhakar <jaanviprabhakar@gmail.com>
2021-01-05 13:53:03 +0530

    Score and endGame function

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Flappy_Bird
             this.pipeBottom = new System.Windows.Forms.PictureBox();
             this.flappyBird = new System.Windows.Forms.PictureBox();
             this.ground = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.scoreText = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
@@ -66,9 +66,9 @@ namespace Flappy_Bird
             // flappyBird
             // 
             this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
-            this.flappyBird.Location = new System.Drawing.Point(89, 167);
+            this.flappyBird.Location = new System.Drawing.Point(21, 89);
             this.flappyBird.Name = "flappyBird";
-            this.flappyBird.Size = new System.Drawing.Size(71, 60);
+            this.flappyBird.Size = new System.Drawing.Size(67, 57);
             this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.flappyBird.TabIndex = 2;
             this.flappyBird.TabStop = false;
@@ -83,17 +83,17 @@ namespace Flappy_Bird
             this.ground.TabIndex = 3;
             this.ground.TabStop = false;
             // 
-            // label1
+            // scoreText
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.PaleGoldenrod;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 49);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Score: 0";
+            this.scoreText.AutoSize = true;
+            this.scoreText.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.scoreText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreText.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(342, 23);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(106, 39);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
             // 
             // gameTimer
             // 
@@ -107,11 +107,11 @@ namespace Flappy_Bird
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(491, 546);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreText);
             this.Controls.Add(this.pipeTop);
             this.Controls.Add(this.ground);
-            this.Controls.Add(this.flappyBird);
             this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.flappyBird);
             this.Name = "Form1";
             this.Text = "Flappy Bird";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
@@ -131,7 +131,7 @@ namespace Flappy_Bird
         private System.Windows.Forms.PictureBox pipeBottom;
         private System.Windows.Forms.PictureBox flappyBird;
         private System.Windows.Forms.PictureBox ground;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreText;
         private System.Windows.Forms.Timer gameTimer;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -13,8 +13,8 @@ namespace Flappy_Bird
     public partial class Form1 : Form
     {
         //variables
-        int pipeSpeed = 8; //default pipe speed
-        int gravity = 10; //default gravity spped
+        int pipeSpeed = 5; //default pipe speed
+        int gravity = 5; //default gravity spped
         int score = 0; //default score set to 0
 
         public Form1()
@@ -27,16 +27,26 @@ namespace Flappy_Bird
             flappyBird.Top += gravity; //add gravity to flappybird; += means it will add the speed of gravity to the picture boxes top location so it will move down
             pipeBottom.Left -= pipeSpeed;  // link the bottom pipes left position to the pipe speed integer, so it will move left with each tick
             pipeTop.Left -= pipeSpeed; // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
+            scoreText.Text = "Score: " + score.ToString();
 
             //checking if any pipe left the screen
             if (pipeBottom.Left < -50)
             {
                 pipeBottom.Left = 800; // for bottom pipe to re- occur
+                score++;
             }
 
-            if(pipeTop.Left < -80)
+            if (pipeTop.Left < -80)
             {
                 pipeTop.Left = 950; // for top pipe to reoccur
+                score++;
+            }
+
+
+            //if the bird hits the ground or touches pipes
+            if (flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) || flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) || flappyBird.Bounds.IntersectsWith(ground.Bounds)|| flappyBird.Top< -20)
+            {
+                endGame(); //if any condition is met, endgame() fn will run
             }
         }
 
@@ -44,7 +54,7 @@ namespace Flappy_Bird
         { // game key is down event linked to main form
             if (e.KeyCode == Keys.Space)
             {
-                gravity = -10; // when spacebar is pressed change gravity to -5 for bird to go up
+                gravity = -5; // when spacebar is pressed change gravity to -5 for bird to go up
             }
 
         }
@@ -53,9 +63,15 @@ namespace Flappy_Bird
         { // game key is up event linked to main form
             if(e.KeyCode == Keys.Space)
             {
-                gravity = 10; //when spacebar is released change it back to 5 for bird to come down
+                gravity = 5; //when spacebar is released change it back to 5 for bird to come down
             }
 
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += "\nGame Over!";
         }
     }
 }
