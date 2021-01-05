8e4679dcd282fe1fab7dce5684ffeae40e8e5ff4
Jaanvi Prabhakar <jaanviprabhakar@gmail.com>
2021-01-05 13:20:00 +0530

    Update Form1.cs
    Adding comments :pencil2:

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,9 @@ namespace Flappy_Bird
     public partial class Form1 : Form
     {
         //variables
-        int pipeSpeed = 8;
-        int gravity = 10;
-        int score = 0;
+        int pipeSpeed = 8; //default pipe speed
+        int gravity = 10; //default gravity spped
+        int score = 0; //default score set to 0
 
         public Form1()
         {
@@ -24,11 +24,12 @@ namespace Flappy_Bird
 
         private void gameTimerEvent(object sender, EventArgs e)
         {
-            flappyBird.Top += gravity; //add gravity
-            pipeBottom.Left -= pipeSpeed;
-            pipeTop.Left -= pipeSpeed;
+            flappyBird.Top += gravity; //add gravity to flappybird; += means it will add the speed of gravity to the picture boxes top location so it will move down
+            pipeBottom.Left -= pipeSpeed;  // link the bottom pipes left position to the pipe speed integer, so it will move left with each tick
+            pipeTop.Left -= pipeSpeed; // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
 
-            if(pipeBottom.Left < -50)
+            //checking if any pipe left the screen
+            if (pipeBottom.Left < -50)
             {
                 pipeBottom.Left = 800; // for bottom pipe to re- occur
             }
@@ -40,8 +41,8 @@ namespace Flappy_Bird
         }
 
         private void gamekeyisdown(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Space)
+        { // game key is down event linked to main form
+            if (e.KeyCode == Keys.Space)
             {
                 gravity = -10; // when spacebar is pressed change gravity to -5 for bird to go up
             }
@@ -49,7 +50,7 @@ namespace Flappy_Bird
         }
 
         private void gamekeyisup(object sender, KeyEventArgs e)
-        {
+        { // game key is up event linked to main form
             if(e.KeyCode == Keys.Space)
             {
                 gravity = 10; //when spacebar is released change it back to 5 for bird to come down
